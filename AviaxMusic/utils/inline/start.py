0f5fa453e716c435476670d2153a6c58f24f2e89
Clients11 <166890312+Clients11@users.noreply.github.com>
2025-05-16 21:44:04 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -6,11 +6,14 @@ from AviaxMusic import app
 
 def start_panel(_):
     buttons = [
+        
         [
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+            ),   
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
     ]
     return buttons
@@ -24,13 +27,8 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
     ]
     return buttons
